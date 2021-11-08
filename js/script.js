e29b2136bda29227af0cdda437494a4148c2c04d
Maurizio <proiettomaurizio97@gmail.com>
2021-11-08 17:03:03 +0100

    update and code refactoring

--- js/script.js
+++ js/script.js
@@ -5,23 +5,12 @@
 // Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
 
 
-
-
-// const dataIcon = [
-// 	{
-// 		name: 'cat',
-// 		prefix: 'fa-',
-// 		type: 'animal',
-// 		family: 'fas',
-// 		color: 'orange'
-// 	},
-
-
 // DONE=> seleziono il container per stampare gli elementi
 // DONE=> creo una funzione per stampare gli elementi in pagina
     // DONE=> scorro gli oggetti dell'array
         // DONE=> utilizzo le proprietà dell'oggetto per inserire le classi dinamicamente
         // DONE=> utilizzo la key "color" per associare la classe che da il colore all'icona
+
 
 // seleziono il contenitore delle icone
 const contIcons = document.querySelector(".container-icons");
@@ -31,43 +20,41 @@ const btn = document.querySelector("button");
 // genero tutte le icone
 iconAll(contIcons, dataIcon);
 // creo un ascoltatore e permetto di scegliere le icone tramite un filtro
-selection.addEventListener("Inputs"/*  "click" */,
-        
+selection.addEventListener("change"/*  "click" */, 
     function () {
         contIcons.innerHTML = " ";
-        dataIcon.forEach((elem) => {
-            const filter = document.querySelector("select").value;
-            if (filter == elem.type) {
-                console.log(`filtro ${filter}`, elem);
-                iconFromFilter(contIcons, elem);
-
-            } else if (filter == "all") {
-                console.log(`filtro ${filter}`, elem);
-                iconFromFilter(contIcons, elem);
-            }
-        }
-        );
-    }
+        const filter = document.querySelector("select").value;
+        // da fare solo se filter è diverso da "all"
+        if(filter !== "all"){
+            const filteredDataIcon = dataIcon.filter((elem) => {
+                if (filter == elem.type) {
+                   return elem;
+                }
                 
-        );
+            }); 
+            iconAll(contIcons, filteredDataIcon);
+
+         // se ha cliccato "all", chiamo iconAll con tutto dataIcon
+        }  else {
+            iconAll(contIcons, dataIcon);
+        }
+    });
+    
 
 // funzioni utili
 // funzione per creare tutte le icone 
 function iconAll(container, myArray) {
     myArray.forEach((elem) => {
-        /* console.log(Elem); */
-
         container.innerHTML +=`
         <div class="icon">
             <i class="fas fa-dog${elem.family} ${elem.prefix}${elem.name} ${elem.color}"></i>
             <span>${elem.name}</span>
         </div>
         `;
-    }
-
-    );
+    });
 };
 
+/*
 // funzione per creare icone scelte tramite filtro
 function iconFromFilter(container, elem) {
     container.innerHTML +=`
@@ -77,3 +64,4 @@ function iconFromFilter(container, elem) {
     </div>
     `;
 };
+*/
